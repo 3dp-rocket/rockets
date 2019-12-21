fe44820649e9d9017fff51db419445cb58e83a25
zwitterion <josesaura@hotmail.com>
2019-12-21 14:45:21 -0800

    adding build scripts

---
+++ janus/stl/38mm/build.sh
@@ -0,0 +1,19 @@
+/home/jose/3P/Prusa/PrusaSlicer.AppImage -g --center 130,100 --output ./gcode --load config.ini retainer.stl
+/home/jose/3P/Prusa/PrusaSlicer.AppImage -g --center 130,100 --output ./gcode --load config.ini motor_mount.stl
+
+
+/home/jose/3P/Prusa/PrusaSlicer.AppImage -g --center 130,100 --output ./gcode --load config.ini coupler2.stl  
+/home/jose/3P/Prusa/PrusaSlicer.AppImage -g --center 130,100 --output ./gcode --load config.ini coupler3.stl  
+/home/jose/3P/Prusa/PrusaSlicer.AppImage -g --center 130,100 --output ./gcode --load config.ini coupler4.stl  
+
+/home/jose/3P/Prusa/PrusaSlicer.AppImage -g --align-xy 0,0 --output ./gcode --load config.ini fin.stl
+
+/home/jose/3P/Prusa/PrusaSlicer.AppImage -g --center 130,100 --output ./gcode --load config.ini motor_mount.stl
+/home/jose/3P/Prusa/PrusaSlicer.AppImage -g --center 130,100 --output ./gcode --load config.ini extension.stl
+/home/jose/3P/Prusa/PrusaSlicer.AppImage -g --center 130,100 --output ./gcode --load config.ini piston.stl
+/home/jose/3P/Prusa/PrusaSlicer.AppImage -g --center 130,100 --output ./gcode --load config.ini piston_screw.stl
+/home/jose/3P/Prusa/PrusaSlicer.AppImage -g --center 130,100 --output ./gcode --load config.ini parachute.stl
+/home/jose/3P/Prusa/PrusaSlicer.AppImage -g --center 130,100 --output ./gcode --load config.ini parachute_middle_extension.stl
+/home/jose/3P/Prusa/PrusaSlicer.AppImage -g --center 130,100 --output ./gcode --load config.ini parachute_extension.stl
+/home/jose/3P/Prusa/PrusaSlicer.AppImage -g --center 130,100 --output ./gcode --load config.ini instrument.stl  
+/home/jose/3P/Prusa/PrusaSlicer.AppImage -g --center 130,100 --output ./gcode --load config.ini nose.stl
